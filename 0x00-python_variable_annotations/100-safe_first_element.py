b4da220d02e21da487b12e4bb5a4f8c38cfdcc44
root <karabo.kmashile@gmail.com>
2024-01-11 14:50:48 -0800

    advanced tasks

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Module for the function safe_first_element """
+
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """takes a sequence of any type as argument and returns any or None"""
+    if lst:
+        return lst[0]
+    else:
+        return None
+

7b359bfa171f389e2fc4543c6bbb1d03a5a7e637
root <karabo.kmashile@gmail.com>
2024-01-15 09:44:42 -0800

    task 0 to 4

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" module for asynchronous coroutine """
+
+import asyncio
+from random import uniform
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    waits for a random delay between 0 and max_delay
+    seconds and eventually returns it
+    """
+    delay = uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
